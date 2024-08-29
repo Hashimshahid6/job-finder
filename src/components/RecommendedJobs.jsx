3c89c1b8ab13fc7afbd59e596d38728f9d7e47e3
Hashimshahid6 <Hashim.shahid60@gmail.com>
2024-08-29 22:46:15 +0500

    styling of categories completed

--- src/components/RecommendedJobs.jsx
+++ src/components/RecommendedJobs.jsx
@@ -12,25 +12,24 @@ const RecommendedJobs = () => {
     );
   };
   return (
-    <div>
-      <div>
-        <div>
-          <h2>Recommended Jobs</h2>
-          <span>
-            <button>Latest Jobs</button>
-            <button>Premium Jobs</button>
+    <div className="bg[#f5f6fc]">
+      <div className="container mx-auto px-6 py-24">
+        <div className="md:flex items-center justify-between">
+          <h2 className="xl:text-5xl lg:text-3xl text-2xl font-semibold">Recommended Jobs</h2>
+          <span className="md:flex gap-x-4">
+            <button className="rounded-full md:my-0 my-4 bg-primary text-lg text-white font-bold px-8 py-3 outline-none border-none hoverBtn">Latest Job</button>
+            <button className="rounded-full bg-transparent border border-solid border[#e2e4e7] text-lg text-black font-bold px-8 py-3 outline-none shadow hoverBtn">Premium Jobs</button>
           </span>
         </div>
-        <p>
-          Explore some of the best jobs from around the world from our wide
-          selection of industries.
+        <p className="text-2xl mt-4 font-light">
+          Explore suggested job searches.
         </p>
-        <div>
-          <div>
-            <div>
-              <h2>Job Categories</h2>
+        <div className="xl:flex gap-x-8 mt-16">
+          <div className="xl:w-2/5">
+            <div className="bg-white rounded-[10px] py-8 pl-6 mb-4">
+              <h2 className="text-4xl font-semibold">Job Categories</h2>
             </div>
-            <div>
+            <div className="flex flex-col gap-y-4">
                 <Categories image="/images/it.png" title="Information Technology" opening="1500" />
                 <Categories image="/images/finance.png" title="Finance" opening="1200" />
                 <Categories image="/images/healthcare.png" title="Healthcare" opening="1200" />
